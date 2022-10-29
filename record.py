710e2e3a284ddf5d7e01958145b45248eb3d97e4
Anthony Costarelli <acostarelli@olin.edu>
2022-10-29 11:23:47 -0400

    this at least works with pre-recorded audio

--- record.py
+++ record.py
@@ -1,40 +1,36 @@
 import pyaudio
 import serial
 import sys
+import time
+import wave
 
 CHUNK = 1024
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 44100
-RECORD_SECONDS = 1
 
 BAUD = 9600
 BUFFER = 256
 
-def listen(in_data, frame_count, time_info, status_flags):
-    audio_data.extend(in_data)
-
-    return (None, pyaudio.paContinue)
-
 if __name__ == "__main__":
     try:
-        arduino, audiofile = sys[1:]
-    except:
+        arduino, audiofile = sys.argv[1:]
+    except ValueError:
         print("Error: Need to specify Arduino port, and optionally a test audio file.")
-        sys.quit(1)
+        sys.exit(1)
 
     pA = pyaudio.PyAudio()
     data = []
 
     if audiofile:
-        with open(audiofile, "rb") as wav:
+        with wave.open(audiofile, "rb") as wav:
             data = wav.readframes(wav.getnframes())
     else:
         def listen(in_data, _, __, ___):
             data.extend(in_data)
             return (None, pyaudio.paContinue)
-        
-        stream = p.open(format=FORMAT,
+
+        stream = pA.open(format=FORMAT,
                         channels=CHANNELS,
                         rate=RATE,
                         input=True,
@@ -43,12 +39,15 @@ if __name__ == "__main__":
         stream.start_stream()
 
     with serial.Serial(arduino, BAUD) as ser:
+    #with open("outputtest.wav", "wb") as ser:
         i = 0
         while True:
-            n = ser.write(audio_data[audio_i + BUFFER])
+            n = ser.write(data[i:i+BUFFER])
 
             i += n
             if n == 0:
                 break
+
+            #time.sleep(0.01)
 
     pA.terminate()
