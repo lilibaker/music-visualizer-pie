e8818b6380e77808652e0140dc21362077ac9ba5
Anthony Costarelli <acostarelli@olin.edu>
2022-11-16 13:53:17 -0500

    it kinda draws n stuff

--- Arduino/sprint2/stepper_idk/stepper_idk.ino
+++ Arduino/sprint2/stepper_idk/stepper_idk.ino
@@ -1,65 +1,74 @@
 #include <Adafruit_MotorShield.h>
-int rad_x = 3; // cm
-int rad_y = 3; // cm
+float rad_x = 0.03; // cm
+float rad_y = 0.03; // cm
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 Adafruit_StepperMotor *YMotor = AFMS.getStepper(200, 1);
 Adafruit_StepperMotor *XMotor = AFMS.getStepper(200, 2);
 
 void setup() {
   Serial.begin(9600);
-  YMotor->setSpeed(100);
-  XMotor->setSpeed(100);
+  YMotor->setSpeed(1000);
+  XMotor->setSpeed(1000);
 
   AFMS.begin();
   Serial.setTimeout(99999);
+  pinMode(LED_BUILTIN, OUTPUT);
 }
 
 int drawing = 0;
-int max_amount = 0;
-int min_amount = 0;
-int x_steps = 0;
-int y_steps = 0;
+float max_amount = 0;
+float min_amount = 0;
+float x_steps = 0;
+float y_steps = 0;
 int draw_i = 0;
-int x_last = 0;
-int y_last = 0;
+float x_last = 0;
+float y_last = 0;
+float temp_x = 0;
+float temp_y = 0;
 int x_dir = 0;
 int y_dir = 0;
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  //XMotor->step(10, FORWARD, MICROSTEP);
-  //YMotor->step(10, FORWARD, MICROSTEP);
+  digitalWrite(LED_BUILTIN, drawing);
   switch(drawing) {
     case 0: {
-      float x = Serial.parseFloat() / (2 * PI * rad_x);// * 200;
-      float y = Serial.parseFloat() / (2 * PI * rad_y);// * 200;
+      float x = Serial.parseFloat() / (2 * PI * rad_x) * 200;
+      float y = Serial.parseFloat() / (2 * PI * rad_y) * 200;
 
       if(x < x_last) {
         x_dir = BACKWARD;
+        temp_x = x;
         x = -x;
       } else {
         x_dir = FORWARD;
+        temp_x = x;
       }
       if(y < y_last) {
         y_dir = BACKWARD;
+        temp_y = y;
         y = -y;
       } else {
         y_dir = FORWARD;
+        temp_y = y;
       }
+      
 
-      min_amount = min(x - x_last, y - y_last);
-      max_amount = max(x - x_last, y - y_last);
-
-      x_last = x;
-      y_last = y;
+      min_amount = min(abs(x - x_last), abs(y - y_last));
+      max_amount = max(abs(x - x_last), abs(y - y_last));
+      if(min_amount == 0) {
+        min_amount = 1;
+      }
 
       x_steps = 1;
       y_steps = 1;
-      if(x == min_amount) {
+      if(abs(x - x_last) == max_amount) {
         x_steps = max_amount / min_amount;
       } else {
         y_steps = max_amount / min_amount;
       }
+
+      x_last = temp_x;
+      y_last = temp_y;
 
       draw_i = 0;
       drawing = 1;
@@ -70,7 +79,7 @@ void loop() {
       XMotor->step(y_steps, y_dir, SINGLE);
       
       draw_i++;
-      if(draw_i == min_amount) {
+      if(draw_i >= min_amount) {
         drawing = 0;
       }
       break;
--- Python/prerecorded.py
+++ Python/prerecorded.py
@@ -7,7 +7,7 @@ from pprint import pprint
 
 BAUD = 9600
 
-def draw(Ri, Ro, q, tmax=100, tstep=1):
+def draw(Ri, Ro, q, tmax=100, tstep=10):
     p = (Ro - Ri) / 2
     q = (Ro - p) * q + p
     R = Ri + p + q
@@ -15,10 +15,17 @@ def draw(Ri, Ro, q, tmax=100, tstep=1):
     x = lambda t: (R - q)*np.cos(t) + p*np.cos(t*(R - q)/q)
     y = lambda t: (R - q)*np.sin(t) - p*np.sin(t*(R - q)/q)
 
-    for t in range(0, tmax, tstep):
-        yield {'x': x(t), 'y': y(t)}
+    xs = np.array([x(t) for t in range(0, tmax, tstep)])
+    ys = np.array([y(t) for t in range(0, tmax, tstep)])
+    xs = list((xs - np.mean(xs)) / (10*np.std(xs)))
+    ys = list((ys - np.mean(ys)) / (10*np.std(ys)))
+    #xs = (xs - np.min(xs)) * 0.5 / (np.min(xs) + np.max(xs)) - 0.25
+    #ys = (ys - np.min(ys)) * 0.5 / (np.min(ys) + np.max(ys)) - 0.25
+
+    return xs, ys
 
 if __name__ == "__main__":
+
     try:
         port, audiofile = sys.argv[1:3]
     except ValueError:
@@ -32,6 +39,8 @@ if __name__ == "__main__":
     avg = list(np.cumsum(avg / np.sum(avg)))
 
     pts = []
+    XS = []
+    YS = []
     for (Ri, Ro), part in zip(itertools.pairwise([0] + avg), parts):
         fqs = np.fft.rfftfreq(part.size, d=1./sr)
         dft = np.fft.rfft(part)
@@ -39,8 +48,28 @@ if __name__ == "__main__":
 
         fq = fqs[np.argmax(dft)]
         q  = fq / 2000
-        pts.extend(list(draw(Ro, Ri, q)))
+        #pts.extend(list(draw(Ro, Ri, q)))
+        xs, ys = draw(Ro, Ri, q)
+        XS = np.array(list(XS) + list(xs))#np.concatenate(XS, xs)
+        YS = np.array(list(YS) + list(ys))#np.concatenate(YS, ys)
 
-    with serial.Serial(port, BAUD) as ser:
-        for pt in pts:
-            ser.write(f"{round(pt['x'], 2)},{round(pt['y'], 2)}".encode())
+    str = ""
+    for x, y in zip(XS, YS):
+        str = str + f"{round(x, 2)},{round(y, 2)} "
+
+    print(str)
+    #pprint(XS)
+    #port = sys.argv[1]
+    #port = "COM9"
+    #with serial.Serial(port, BAUD) as ser:
+        #for x, y in zip(XS, YS):
+        #    ser.write(f"{round(x, 2)},{round(y, 2)}\n".encode())
+            #print(f"{round(x, 2)},{round(y, 2)}")
+
+        #for i in range(61):
+        #    ser.write(f"100,100\n".encode())
+        #ser.write("0.03,0.03\n".encode())
+        #for x, y in zip(XS, YS):
+        #    ser.write(f"{round(x, 2)},{round(y, 2)}\n".encode())a
+        #ser.write("100,100\n".encode())
+        #ser.write("100,100\n".encode())
