07cfcf99d6f1289f11c5d841844d63ecd3a45a2d
lilibaker <65792993+lilibaker@users.noreply.github.com>
2022-12-01 16:30:51 -0500

    Add dropdown after searching song

--- Python/interface.py
+++ Python/interface.py
@@ -1,4 +1,3 @@
-# import os
 import tkinter as tk
 from tkinter import ttk
 from ttkthemes import ThemedTk
@@ -16,9 +15,10 @@ sp = spotipy.Spotify(
 # define global veriables
 RESULTS = 0
 features = []
+options = []
 
 
-# set up window
+# set up window with theme
 root = ThemedTk(theme="breeze")
 root.title("Music Visualizer")
 root.geometry("800x800")
@@ -29,72 +29,97 @@ frmMain.grid_columnconfigure(0, weight=1)
 root.grid_rowconfigure(0, weight=1)
 root.grid_columnconfigure(0, weight=1)
 
-# add title
+# add title to window
 heading = ttk.Label(frmMain, text="Music Visualizer", font=("Arial", 25))
 heading.grid(row=0, column = 0, pady = 7)
 
-# # Add instructions
-# instructions = ttk.Label(frmMain, 
-#                         text="Choose a song to visualize from the dropdown below, or enter a different song name in the box below!",
-#                         font=("Arial", 12),
-#                         wraplength=500,
-#                         justify="center")
-# instructions.grid(row=1, column = 0, pady = 7)
-
-# # change label text
-# def show():
-#     label.config(text=clicked.get())
-
-# def draw_spiro():
-#     print("value is: " + clicked.get() + ".mp3")
-#     os.system("python3 prerecorded.py COM5" + clicked.get() + ".mp3")
-    
-
-# #dropdown options
-# options = [
-#     "Her Majesty",
-#     "Parachutes",
-#     "Stop"
-# ]
-
-# clicked = tk.StringVar() # datatype of menu text
-# clicked.set(options[0]) # set initial text
-# drop = ttk.OptionMenu(frmMain, clicked, *options)
-# drop.grid(row=2, column = 0, pady = 7)
-# button = ttk.Button(frmMain, text=" ")
-# label = ttk.Label(frmMain, text=" ")
-# label.grid(row=3, column = 0, pady = 7)
-
-# # button to send data and run prerecorded.py
-# draw = ttk.Button(frmMain, text="Draw", command=draw_spiro)
-# draw.grid(row=4, column = 0, pady = 7)
+# set up dropdown for songs later
+clicked = tk.StringVar() # datatype of menu text
+clicked.set("Choose a song") # set initial text
 
 # download music 
 def displayArtists():
+    """
+    Display the first five songs that match the song the user enter in
+    the text box.
+    """
+    # get the song name the user entered; should be a string
     name = songName.get(1.0, "end-1c")
-    artists = ""
+
+    # search the name of the song using spotify api
     global RESULTS
     RESULTS = sp.search(name, limit=5)
 
+    # save the first five songs with artist names to options
+    global options
     for i, track in enumerate(RESULTS["tracks"]["items"]):
-        artists += f"[{i}] {track['name']} by {track['artists'][0]['name']}\n"
+        options.append(f"[{i}] {track['name']} by {track['artists'][0]['name']}")
     
-    artistNames.config(text = artists)
+    # update screen
+    updateScreen()
+
+def updateScreen():
+    """
+    Update the screen to have a dropdown with list of songs and a button to draw.
+    """
+    
+    # artist name instructions
+    artistInstructions = ttk.Label(frmMain, 
+                                    text="Once the list of songs appears, select the song you want and click done!",
+                                    font=("Arial", 12),
+                                    wraplength=500,
+                                    justify="center")
+    artistInstructions.grid(row=8, column = 0, pady = 7)
+
+    # send artists to dropdown and display
+    drop = ttk.OptionMenu(frmMain, clicked, options[0], *options)
+    drop.grid(row=10, column = 0, pady = 7)
+    button = ttk.Button(frmMain, text=" ")
+    label = ttk.Label(frmMain, text=" ")
+    label.grid(row=11, column = 0, pady = 7)
+    
+    # button to search name of song
+    download = ttk.Button(frmMain, text="Done", command=getFeatures)
+    download.grid(row=12, column = 0, pady = 7)
 
 def getFeatures():
-    id = int(artistID.get(1.0, "end-1c"))
+    """
+    Get the audio features of the song the user choices and call the
+    'visualizeMusic' function.
+    """
+    # get the id number associated with the song selected in the dropdown
+    id = int(clicked.get()[1])
     id = RESULTS["tracks"]["items"][id]["id"]
+
+    # use spotify api to get the audiofeatures of the song
     results = sp.audio_features(id)
+    
+    # store the values of the desired features in features
     global features
     i = 0
     for feature in ["danceability", "key", "loudness", "mode",\
         "acousticness", "instrumentalness", "liveness", "valence", \
-        "tempo"]:
+        "energy"]:
         features.append(float(results[0][feature]))
+    
+    # call the visualize function
     visualizeMusic(features)
 
 def visualizeMusic(features):
-    # run prerecorded with features
+    """
+    Use the feature values to visualize music. Calls the prerecorded.draw function.
+
+    Parameters:
+        features: Array of floats representing the song's feature values
+            on a scale of 0 to 1.
+    """
+
+    # scale key so it is between 0 and 1; the normal range is -1 to 11
+    features[1] = (features[1] + 1) / 12
+    # scale loudness to 0 to 1; normal range is -60 to 0
+    features[2] = features[2] * -1 / 60
+
+    # draw with updated values
     prerecorded.draw(features[0], features[1], features[2])
     prerecorded.draw(features[3], features[4], features[5])
     prerecorded.draw(features[6], features[7], features[8])
@@ -106,30 +131,13 @@ songInstructions = ttk.Label(frmMain,
                             wraplength=500,
                             justify="center")
 songInstructions.grid(row=5, column = 0, pady = 7)
-# text input
+
+# text input for song
 songName = tk.Text(frmMain, height=5, width=40)
 songName.grid(row=6, column = 0, pady = 7)
+
 # button to search name of song
 searchName = ttk.Button(frmMain, text="Search Song", command=displayArtists)
 searchName.grid(row=7, column = 0, pady = 7)
-
-# artist name instructions
-artistInstructions = ttk.Label(frmMain, 
-                                text="Once the list of songs appears, type the number corresponding to the song you want and click done!",
-                                font=("Arial", 12),
-                                wraplength=500,
-                                justify="center")
-artistInstructions.grid(row=8, column = 0, pady = 7)
-# label for artist names
-artistNames = ttk.Label(frmMain, text="")
-artistNames.grid(row=9, column = 0, pady = 7)
-
-# artist text input
-artistID = tk.Text(frmMain, height=5, width=40)
-artistID.grid(row=10, column = 0, pady = 7)
-# button to search name of song
-download = ttk.Button(frmMain, text="Done", command=getFeatures)
-download.grid(row=11, column = 0, pady = 7)
-
 
 root.mainloop()
