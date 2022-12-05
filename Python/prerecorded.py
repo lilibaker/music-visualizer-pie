f8d1d5f3ee20cdbe55373b8368937ade1102383a
Anthony Costarelli <acostarelli@olin.edu>
2022-12-05 15:14:56 -0500

    now normalize position data using euclidean norm; draw functino returns relative positions, not abolute now

--- Python/prerecorded.py
+++ Python/prerecorded.py
@@ -1,5 +1,5 @@
 import itertools
-import numpy as np
+import math
 from scipy.io import wavfile
 import serial
 import sys
@@ -7,24 +7,25 @@ from pprint import pprint
 
 BAUD = 9600
 
-def draw(Ri, Ro, q, tmax=100, tstep=10):
-    min_r = min(Ri, Ro)
-    Ro = max(Ri, Ro)
-    Ri = min_r
+def norm(v):
+    return math.sqrt(sum(e**2 for e in v))
+
+def draw2(Ri, Ro, q, tmax=100, tstep=10):
+    Ro, Ri = max(Ri, Ro), min(Ri, Ro)
     p = (Ro - Ri) / 2
     q = (Ro - p) * q + p
     R = Ri + p + q
     q = min(0.1, q)
 
-    x = lambda t: (R - q)*np.cos(t) + p*np.cos(t*(R - q)/q)
-    y = lambda t: (R - q)*np.sin(t) - p*np.sin(t*(R - q)/q)
+    x = lambda t: (R - q)*math.cos(t) + p*math.cos(t*(R - q)/q)
+    y = lambda t: (R - q)*math.sin(t) - p*math.sin(t*(R - q)/q)
 
-    xs = np.array([x(t) for t in range(0, tmax, tstep)])
-    ys = np.array([y(t) for t in range(0, tmax, tstep)])
-    xs = list((xs - np.mean(xs)) / (10*np.std(xs)))
-    ys = list((ys - np.mean(ys)) / (10*np.std(ys)))
-    #xs = (xs - np.min(xs)) * 0.5 / (np.min(xs) + np.max(xs)) - 0.25
-    #ys = (ys - np.min(ys)) * 0.5 / (np.min(ys) + np.max(ys)) - 0.25
+    xs = [x(t) for t in range(0, tmax, tstep)]
+    ys = [y(t) for t in range(0, tmax, tstep)]
+    xs = [x / norm(xs) for x in xs]
+    ys = [y / norm(ys) for y in ys]
+    xs = [b - a for a, b in itertools.pairwise(xs)]
+    ys = [b - a for a, b in itertools.pairwise(ys)]
 
     return xs, ys
 
